6e35a641c12ace14ece611e661abedc645e8ecd6
Marc Tönsing <marc@marc.tv>
2019-06-13 23:41:53 +0200

    Native Lazy oembed

--- native-lazy-oembed.php
+++ native-lazy-oembed.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
-  Plugin Name:Oembed native Lazy
+  Plugin Name: Native Lazy oembed
   Plugin URI: http://marc.tv/
   Description: Adds loading=lazy attribute to all oembed iframnes.
   Version: 1.0
   Author: MarcDK
   Author URI: https://marc.tv
-  GitHub Plugin URI: mtoensing/marctv-reply-focus
+  GitHub Plugin URI: mtoensing/native-lazy-oembed
   License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
   This program is free software; you can redistribute it and/or modify
@@ -22,10 +22,9 @@
 
  */
 
- function lazy_embed_oembed_html($html, $url, $attr)
- {
-  $html = str_replace('<iframe ','<iframe loading="lazy" ',$html);
-     return $html;
+ function lazy_embed_oembed_html($html, $url, $attr){
+    $html = str_replace('<iframe ','<iframe loading="lazy" ',$html);
+    return $html;
  }
 
  add_filter('embed_oembed_html', 'lazy_embed_oembed_html',10, 3);
